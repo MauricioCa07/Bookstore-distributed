8628e73a4676651596386cf7aa381600c487465c
MauricioCa07 <carrillomauricio007@gmail.com>
2025-05-16 15:32:53 -0500

    changing database

--- Catalog/config.py
+++ Catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:123456789admin@database-1.cbdlmmsx8z7q.us-east-1.rds.amazonaws.com/bookstore'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://database-2:admin123456789@database-2.cbdlmmsx8z7q.us-east-1.rds.amazonaws.com/bookstore'
 SECRET_KEY = 'secretkey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
--- Transactions/config.py
+++ Transactions/config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:123456789admin@database-1.cbdlmmsx8z7q.us-east-1.rds.amazonaws.com/bookstore'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://database-1:admin123456789@database-2.cbdlmmsx8z7q.us-east-1.rds.amazonaws.com/bookstore'
 SECRET_KEY = 'secretkey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
