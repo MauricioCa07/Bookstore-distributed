8297b2306222b1e0f0f68fde90659945694609e0
MauricioCa07 <carrillomauricio007@gmail.com>
2025-05-16 16:12:39 -0500

    adding a database

--- Authentication/config.py
+++ Authentication/config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://database-2:admin123456789@database-2.cbdlmmsx8z7q.us-east-1.rds.amazonaws.com:3306/bookstore'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://database-3:admin123456789@database-3.cbdlmmsx8z7q.us-east-1.rds.amazonaws.com:3306/bookstore'
 SECRET_KEY = 'secretkey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
